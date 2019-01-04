3068ce02e802ccfd94f004daa02c9dd0c9b3cd98
pr0x1 <yamit.huertas@gmail.com>
2019-01-04 06:23:39 -0500

    lectura activity e intents de navegación

--- app/src/main/java/com/a4app/develop/traslados/TransporteActivity.java
+++ app/src/main/java/com/a4app/develop/traslados/TransporteActivity.java
@@ -1,7 +1,9 @@
 package com.a4app.develop.traslados;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class TransporteActivity extends AppCompatActivity {
 
@@ -9,5 +11,22 @@ public class TransporteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transporte);
+    }
+
+    public void goLecturaActivity(View vista){
+        Intent i = new Intent(this, LecturaActivity.class);
+        /*if (texto.equals("") || texto.equals(null))
+            i.putExtra("texto", "TEXTO VACÍO");
+        else
+            i.putExtra("texto", texto);*/
+        startActivity(i);
+    }
+    public void goCentrosActivity(View vista){
+        Intent i = new Intent(this, CentrosActivity.class);
+        /*if (texto.equals("") || texto.equals(null))
+            i.putExtra("texto", "TEXTO VACÍO");
+        else
+            i.putExtra("texto", texto);*/
+        startActivity(i);
     }
 }
